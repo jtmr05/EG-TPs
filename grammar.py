f908e0bdd52a052695a48eb129a06eb07adedc25
jtmr05 <62111562+jtmr05@users.noreply.github.com>
2023-04-09 00:27:23 +0100

    name fix

--- grammar.py
+++ grammar.py
@@ -92,18 +92,18 @@ tuple_literal  : "(" (expression ("," expression)*)? ")"
 '''
 
 
-def annotate(src: typing.Any, *ascii_escape_codes: int) -> str:
+def annotate(src: typing.Any, *ansi_escape_codes: int) -> str:
 
-    length: int = len(ascii_escape_codes)
+    length: int = len(ansi_escape_codes)
     if length == 0:
         return str(src)
 
     import io
     str_buffer: io.StringIO = io.StringIO()
 
-    str_buffer.write(f"\033[{ascii_escape_codes[0]}")
+    str_buffer.write(f"\033[{ansi_escape_codes[0]}")
     for i in range(1, length):
-        str_buffer.write(f";{ascii_escape_codes[i]}")
+        str_buffer.write(f";{ansi_escape_codes[i]}")
     str_buffer.write(f"m{str(src)}\033[0m")
 
     return str_buffer.getvalue()
